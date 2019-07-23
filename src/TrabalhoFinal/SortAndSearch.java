ef25711ffbc556cba0011c032a2a4d359359690e
Nuno Silva <nuno.s.silva@gmail.com>
2019-07-23 11:51:19 +0100

    SortandSearch  add sort

--- src/TrabalhoFinal/SortAndSearch.java
+++ src/TrabalhoFinal/SortAndSearch.java
@@ -1,19 +1,40 @@
 package TrabalhoFinal;
 
 public class SortAndSearch {
-	
-	public static <T extends Comparable<? super T>> boolean linearSearch2(Node<T> head, T target) {
 
-		Node<T> current = head; 
-		
-		boolean found = false;
-		while (!found && current != null) {
-			if (current.getElement().compareTo(target) == 0)
-				found = true;
-			current = current.getNext();
-		}
-		return found;
+    public static <T extends Comparable<? super T>> boolean linearSearch2(Node<T> head, T target) {
 
-	}
+        Node<T> current = head;
+
+        boolean found = false;
+        while (!found && current != null) {
+            if (current.getElement().compareTo(target) == 0) {
+                found = true;
+            }
+            current = current.getNext();
+        }
+        return found;
+
+    }
+
+    public static <T extends Comparable<? super T>> void selectionSort(Node<T> head) {
+        Node<T> temp;
+
+        for (Node<T> current = head; current.getNext() != null; current.getNext()) {
+            Node<T> min = current;
+
+            for (Node<T> scan = current.getNext(); scan != null; scan = scan.getNext()) {
+                if (scan.getElement().compareTo(min.getElement()) < 0) {
+                    min = scan;
+                }
+            }
+            if (min != current) {
+                temp = new Node<T>(min.getElement());
+                min.setElement(current.getElement());
+                current.setElement(temp.getElement());
+            }
+
+        }
+    }
 
 }
